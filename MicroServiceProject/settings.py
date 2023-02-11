41a4ff5795306f7d100ef34967a2a7610633ef15
vionaosiako <viona.osiako@student.moringaschool.com>
2023-02-12 00:43:14 +0300

    patch:change allowed host

--- MicroServiceProject/settings.py
+++ MicroServiceProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['microservice-program-production.up.railway.app']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
